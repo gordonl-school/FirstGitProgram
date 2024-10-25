5a1bb3f01aae7c1fb5d9e304cd5eb6bf938943ee
BT_1N3_03 <sample@bths.edu>
2024-10-25 13:12:18 -0400

    Added one more print, created variables, and used those variables in string concatenation

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,8 @@ public class Runner {
         System.out.println("Today is Friday!");
         System.out.println("I want to relax");
         System.out.println("However, I have three tests next week, which is very sad.");
+        int x = 3;
+        int y = 1;
+        System.out.println("On Wednesday I have an AP Lang test and once I finish that, I only have " + (x - y) + " tests left. ");
     }
 }
