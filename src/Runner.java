99c18638c7c91a5d5bb5c66456a258b50e2e61cf
BT_1N3_03 <sample@bths.edu>
2024-10-25 13:10:38 -0400

    Change and added print

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Today is Friday!");
-        System.out.println("I can't wait to relax!");
+        System.out.println("I want to relax");
+        System.out.println("However, I have three tests next week, which is very sad.");
     }
 }
